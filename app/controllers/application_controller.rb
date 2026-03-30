531d5a91ee5e458a7b8b52e2088b520900c8dcff
jeremyting0727 <jeremy.tt0727@gmail.com>
2026-03-30 23:39:35 +0800

    main update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,26 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   stale_when_importmap_changes
 
+  before_action :authenticate_user!
   before_action :set_terminal_nav_context
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :show_admin_league_actions?
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   def set_terminal_nav_context
     @nav_league = League.order(:id).first
-    demo = User.find_by(name: "Demo Trader")
-    @nav_portfolio = demo&.portfolios&.first || Portfolio.order(:id).first
+    @nav_portfolio = current_user&.portfolios&.first || Portfolio.order(:id).first
   end
 
   def show_admin_league_actions?
-    uid = session[:user_id]
-    return true if uid.blank?
-    User.find_by(id: uid)&.role.to_s == "admin"
+    return false unless current_user
+    current_user.role.to_s == "admin"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :league_memberships, dependent: :destroy
   has_many :leagues, through: :league_memberships
   has_many :portfolios, dependent: :destroy
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    errors.add :password, "must include at least one lowercase letter" unless password.match?(/[a-z]/)
+    errors.add :password, "must include at least one uppercase letter" unless password.match?(/[A-Z]/)
+    errors.add :password, "must include at least one digit" unless password.match?(/\d/)
+    errors.add :password, "must include at least one special character (!@#$%^&*)" unless password.match?(/[!@#$%^&*]/)
+  end
 end
