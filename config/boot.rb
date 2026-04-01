e7b9d9a8f3012200aa87a68a4210225f648cc266
Ethannggggg <ethan0725@gmail.com>
2026-04-02 00:13:11 +0800

    fix: update loofah to 2.25.1 to fix security vulnerability

--- config/boot.rb
+++ config/boot.rb
@@ -8,10 +8,5 @@ env_file = File.expand_path("../.env", __dir__)
 rails_env = ENV.fetch("RAILS_ENV", ENV.fetch("RACK_ENV", "development"))
 if rails_env == "development" && !ENV["CI"] && File.exist?(env_file)
   require "dotenv"
-<<<<<<< HEAD
   Dotenv.load(env_file)
-=======
-  env_path = File.expand_path("../.env", __dir__)
-  Dotenv.load(env_path) if File.exist?(env_path)
->>>>>>> 67b1954 (Update config/boot.rb)
 end
